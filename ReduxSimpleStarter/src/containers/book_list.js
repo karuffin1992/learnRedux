a0d8402c178e5225f93b0a12a8cb2ae21c48d916
Kevin Ruffin <karuffin1992@gmail.com>
2018-09-25 20:07:06 -0400

    Finished first container; updated spelling mistakes; implemented container in App

--- ReduxSimpleStarter/src/containers/book_list.js
+++ ReduxSimpleStarter/src/containers/book_list.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 // Container - react component that has direct connection
 // to state managed by Redux.
@@ -8,21 +9,46 @@ import React, { Component } from 'react';
 
 // Separting containers from components.
 
-export default class BookList extends Component {
+// Import connect from react-redux (note the lowercase 'c')
+
+// Whenever the application state changes, this container is rerendered
+// with a new set of books.
+
+class BookList extends Component {
   renderList() {
+    // Want to get list of books
     return this.props.books.map((book) => {
       return (
         <li
-          key={ book.title } className="list-group-item">{ book.title }</li>
+          key={ book.title }
+          className="list-group-item"
+        >{ book.title }</li>
       );
     });
   }
 
   render() {
-    return {
+    return (
       <ul className="list-group col-sm-4">
         { this.renderList() }
       </ul>
-    }
+    )
   }
 }
+
+// Take app state and whatever is returned
+// will show up as props inside of BookList
+//    return {
+//      asdf: '123'
+//    }
+//    console.log(this.props.asdf); ----> '123'
+
+// The object returns an object that is used in the BookList component
+function mapStateToProps(state) {
+  return {
+    books: state.books // == this.state.props;
+  }
+}
+
+// connect (from react-redux) takes a function and a component and creates a container
+export default connect(mapStateToProps)(BookList);
--- ReduxSimpleStarter/src/reducers/reducer_books.js
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -1,4 +1,4 @@
-export default funciton() {
+export default function() {
   return [
     { title: 'Javascript: The Good Parts' },
     { title: 'Harry Potter' },
